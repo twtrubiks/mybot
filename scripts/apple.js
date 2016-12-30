59ab3d817714558d3d8fc2e3e276420a7eecebc0
twtrubiks <twtrubiks@gmail.com>
2016-12-30 16:27:02 +0800

    add robot.catchAll

--- scripts/apple.js
+++ scripts/apple.js
@@ -35,4 +35,9 @@ module.exports = function(robot) {
 			}		
         })		
     });
+	return robot.catchAll(function(res) {
+    return res.send("您輸入的指令 "+ res.message.text + " 不存在哦~ " +
+	"\n如需查看更多的指令，請輸入 bot help");
+  });
+	
 }
